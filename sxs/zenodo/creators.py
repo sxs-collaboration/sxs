1829941a03500bb91cb7ced2ba555edda63a2700
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-03 13:46:21 -0400

    Add all authors currently in repo to creators

--- sxs/zenodo/creators.py
+++ sxs/zenodo/creators.py
@@ -1,7 +1,55 @@
-creators = [
-    {
-        'name': 'Boyle, Michael',
-        'affiliation': 'Cornell University',
-        'orcid': '0000-0002-5075-5116',
-    },
-]
+boyle = {
+    'name': 'Boyle, Michael',
+    'affiliation': 'Cornell University',
+    'orcid': '0000-0002-5075-5116',
+}
+buchman = {
+    'name': 'Buchman, Luisa',
+    'affiliation': 'Caltech',
+}
+duez = {
+    'name': 'Duez, Matthew',
+    'affiliation': 'Washington State',
+}
+foucart = {
+    'name': 'Foucart, Francois',
+    'affiliation': 'University of New Hampshire',
+}
+grudich = {
+    'name': 'Grudich, Michael',
+    'affiliation': 'Caltech',
+}
+hemberger = {
+    'name': 'Hemberger, Dan',
+}
+kidder = {
+    'name': 'Kidder, Larry',
+    'affiliation': 'Cornell University',
+}
+lovelace = {
+    'name': 'Lovelace, Geoffrey',
+    'affiliation': 'Cal State Fullerton',
+}
+macdonald = {
+    'name': 'MacDonald, Ilana',
+}
+mroue = {
+    'name': 'Mroue, Abdul',
+}
+pfeiffer = {
+    'name': 'Pfeiffer, Harald',
+    'affiliation': 'AEI Potsdam',
+}
+scheel = {
+    'name': 'Scheel, Mark',
+    'affiliation': 'Caltech',
+}
+szilagyi = {
+    'name': 'Szilagyi, Bela',
+}
+taylor = {
+    'name': 'Taylor, Nicholas',
+}
+zenginoglu = {
+    'name': 'Zenginoglu, Anil',
+}
