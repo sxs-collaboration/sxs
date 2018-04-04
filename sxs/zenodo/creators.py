92b1b862a36ac7245b414675afe4805c37752662
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-03 20:05:55 -0400

    Reformat creators

--- sxs/zenodo/creators.py
+++ sxs/zenodo/creators.py
@@ -1,55 +1,57 @@
-boyle = {
-    'name': 'Boyle, Michael',
-    'affiliation': 'Cornell University',
-    'orcid': '0000-0002-5075-5116',
-}
-buchman = {
-    'name': 'Buchman, Luisa',
-    'affiliation': 'Caltech',
-}
-duez = {
-    'name': 'Duez, Matthew',
-    'affiliation': 'Washington State',
-}
-foucart = {
-    'name': 'Foucart, Francois',
-    'affiliation': 'University of New Hampshire',
-}
-grudich = {
-    'name': 'Grudich, Michael',
-    'affiliation': 'Caltech',
-}
-hemberger = {
-    'name': 'Hemberger, Dan',
-}
-kidder = {
-    'name': 'Kidder, Larry',
-    'affiliation': 'Cornell University',
-}
-lovelace = {
-    'name': 'Lovelace, Geoffrey',
-    'affiliation': 'Cal State Fullerton',
-}
-macdonald = {
-    'name': 'MacDonald, Ilana',
-}
-mroue = {
-    'name': 'Mroue, Abdul',
-}
-pfeiffer = {
-    'name': 'Pfeiffer, Harald',
-    'affiliation': 'AEI Potsdam',
-}
-scheel = {
-    'name': 'Scheel, Mark',
-    'affiliation': 'Caltech',
-}
-szilagyi = {
-    'name': 'Szilagyi, Bela',
-}
-taylor = {
-    'name': 'Taylor, Nicholas',
-}
-zenginoglu = {
-    'name': 'Zenginoglu, Anil',
+known_creators = {
+    'Michael Boyle': {
+        'name': 'Boyle, Michael',
+        'affiliation': 'Cornell University',
+        'orcid': '0000-0002-5075-5116',
+    },
+    'Luisa Buchman': {
+        'name': 'Buchman, Luisa',
+        'affiliation': 'Caltech',
+    },
+    'Matthew Duez': {
+        'name': 'Duez, Matthew',
+        'affiliation': 'Washington State',
+    },
+    'Francois Foucart': {
+        'name': 'Foucart, Francois',
+        'affiliation': 'University of New Hampshire',
+    },
+    'Michael Grudich': {
+        'name': 'Grudich, Michael',
+        'affiliation': 'Caltech',
+    },
+    'Dan Hemberger': {
+        'name': 'Hemberger, Dan',
+    },
+    'Larry Kidder': {
+        'name': 'Kidder, Larry',
+        'affiliation': 'Cornell University',
+    },
+    'Geoffrey Lovelace': {
+        'name': 'Lovelace, Geoffrey',
+        'affiliation': 'Cal State Fullerton',
+    },
+    'Ilana MacDonald': {
+        'name': 'MacDonald, Ilana',
+    },
+    'Abdul Mroue': {
+        'name': 'Mroue, Abdul',
+    },
+    'Harald Pfeiffer': {
+        'name': 'Pfeiffer, Harald',
+        'affiliation': 'AEI Potsdam',
+    },
+    'Mark Scheel': {
+        'name': 'Scheel, Mark',
+        'affiliation': 'Caltech',
+    },
+    'Bela Szilagyi': {
+        'name': 'Szilagyi, Bela',
+    },
+    'Nicholas Taylor': {
+        'name': 'Taylor, Nicholas',
+    },
+    'Anil Zenginoglu': {
+        'name': 'Zenginoglu, Anil',
+    }
 }
