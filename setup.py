6d6d7cf6698eae8fd29da04a3fb4fd570b757c44
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-26 22:38:58 -0400

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,82 +1,53 @@
-from setuptools import setup
-from distutils.command.build_py import build_py
+#!/usr/bin/env python
 
+# Copyright (c) 2018, Michael Boyle
+# See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
 
-def calculate_version():
-    """Construct a version string from date, time, and short git hash
-
-    The resulting version looks something like "2018.4.11.12.56.27.dev71379535", where the first six
-    numbers are the date and time, while the part after "dev" is the short git hash converted to
-    decimal.  This version string has a few nice features:
-      * This is a valid PEP 440 version number
-      * It's automatic
-      * It's serial
-      * It contains all the information I can think of that might be needed
-
-    Unfortunately, in order to comply with PEP 440 (and more specifically, not break various python
-    tools that analyze version numbers), the short git hash must be converted from hexadecimal to
-    decimal.  To convert it back, you can run a python command like
-
-        >>> hex(71379535)[2:]
-        '4412a4f'
-
-    """
-    import subprocess
-    from warnings import warn
-    from sys import platform
-    on_windows = ('win' in platform.lower() and not 'darwin' in platform.lower())
-    use_shell = not on_windows
+# Construct the version number from the date and time this python version was created.
+from os import environ
+if "package_version" in environ:
+    version = environ["package_version"]
+    print("Setup.py using environment version='{0}'".format(version))
+else:
+    print("The variable 'package_version' was not present in the environment")
     try:
-        git_revision = subprocess.check_output("""git show -s --format="%ci %h" HEAD""", shell=use_shell).decode('ascii').rstrip()
-        date, time, utc_offset, short_hash = git_revision.split(' ')
-        date = date.replace('-', '.').strip()  # make date an acceptable version string
-        time = time.replace(':', '.').strip()  # make time an acceptable version string
-        short_hash = short_hash.strip()  # remove newline and any other whitespace
-        short_hash = int(short_hash, 16)  # So that it's a valid PEP 440 version identifier
-        dirty = bool(subprocess.call("git diff-files --quiet --", shell=use_shell))
-        dirty = dirty or bool(subprocess.call("git diff-index --cached --quiet HEAD --", shell=use_shell))
-        version = '{0}.{1}.dev{2}'.format(date, time, short_hash)
-        if dirty:
-            version += '+dirty'
-        exec('putative__version__ = "{0}"'.format(version))  # see if this will raise an error for some reason
-    except Exception as e:
-        # If any of the above failed for any reason whatsoever, fall back on this dumb version
-        from os import getenv
-        from datetime import datetime
-        warning = ('\nThe `calculate_version` function failed to get the git version.'
-                   'Maybe your version of python (<2.7?) is too old.  Here\'s the exception:\n' + str(e) + '\n'
-                   'This should not be a problem, unless you need an accurate version number.'
-                   'Continuing on, in spite of it all...\n')
-        warn(warning)
-        date, time = datetime.now().isoformat().split('T')
-        date = date.replace('-', '.').strip()
-        time = time[:8].replace(':', '.').strip()
-        version = '0.0.0.dev' + date + '.' + time
-    return version
+        # For cases where this is being installed from git.  This gives the true version number.
+        from sys import platform
+        from subprocess import check_output
+        on_windows = ('win' in platform.lower() and not 'darwin' in platform.lower())
+        use_shell = not on_windows
+        version = check_output("""git log -1 --format=%cd --date=format:'%Y.%-m.%-d.%-H.%-M.%-S'""", shell=use_shell).decode('ascii').rstrip()
+        print("Setup.py using git log version='{0}'".format(version))
+    except:
+        # For cases where this isn't being installed from git.  This gives the wrong version number,
+        # but at least it provides some information.
+        try:
+            from time import strftime, gmtime
+            try:
+                version = strftime("%Y.%-m.%-d.%-H.%-M.%-S", gmtime())
+            except ValueError:  # because Windows
+                version = strftime("%Y.%m.%d.%H.%M.%S", gmtime())
+            print("Setup.py using strftime version='{0}'".format(version))
+        except:
+            version = '0.0.0'
+            print("Setup.py failed to determine the version; using '{0}'".format(version))
+with open('_version.py', 'w') as f:
+    f.write('__version__ = "{0}"'.format(version))
 
 
-class build_py_and_copy_version(build_py):
-    """Add version-copying step to standard build_py"""
-    def run(self):
-        build_py.run(self)
-        version = calculate_version()
-        print('build_py_copy_version using __version__ = "{0}"'.format(version))
-        if not self.dry_run:
-            import os.path
-            for package in self.packages:
-                with open(os.path.join(self.build_lib, os.path.join(*package.split('.')), '_version.py'), 'w') as fobj:
-                    fobj.write('__version__ = "{0}"'.format(version))
+# long_description = """"""
 
-
-setup(
-    name = 'sxs',
-    packages = ['sxs', 'sxs.metadata', 'sxs.doxygen', 'sxs.references', 'sxs.zenodo', 'sxs.zenodo.api'],
-    scripts = ['scripts/sxs'],
-    version=calculate_version(),
-    cmdclass={'build_py': build_py_and_copy_version},
-    description = 'A collection of python code used by the SXS collaboration',
-    author = 'SXS Collaboration',
-    author_email = 'web-admin@black-holes.org',
-    url = 'https://www.black-holes.org/',
-    # download_url = 'https://github.com/moble/sxs/archive/master.tar.gz',
-)
+if __name__ == "__main__":
+    from os import getenv
+    from setuptools import setup
+    setup(name='sxs',
+          packages = ['sxs', 'sxs.metadata', 'sxs.doxygen', 'sxs.references', 'sxs.zenodo', 'sxs.zenodo.api'],
+          scripts = ['scripts/sxs'],
+          version=version,
+          description = 'A collection of python code used by the SXS collaboration',
+          url='https://github.com/moble/sxs',
+          author='Michael Boyle',
+          author_email='mob22@cornell.edu',
+          # long_description=long_description,
+          # download_url = 'https://github.com/moble/sxs/archive/master.tar.gz',
+    )
