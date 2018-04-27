97ca0eb11388e24983338fb62ab002974b8cd5cc
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-04-27 12:15:54 -0400

    Don't expose utilities at high level (reduces confusion)

--- sxs/zenodo/api/__init__.py
+++ sxs/zenodo/api/__init__.py
@@ -1,7 +1,7 @@
 from .login import Login
 from .deposit import Deposit
 from .records import Records
-from .utilities import md5checksum, find_files
+# from .utilities import md5checksum, find_files
 
 url_standard = 'https://zenodo.org/'
 url_sandbox = 'https://sandbox.zenodo.org/'
