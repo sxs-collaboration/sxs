03a4cb70fb8ee87600d5684a789a043b850fea5a
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-05-07 12:55:37 -0400

    Updating the catalog layout

--- sxs/zenodo/catalog.py
+++ sxs/zenodo/catalog.py
@@ -7,11 +7,147 @@ catalog_file_description = """
 
           {
               'catalog_file_description': '<this description>',
-              'last_file_modification': '<YYYY-MM-DDThh:mm:ss.ssssss>',  # UTC time of last modification made to this file
+              'modified': '<YYYY-MM-DDThh:mm:ss.ssssss>',  # UTC time of last-modified record in this file
+              'records': {  # Includes *all* versions of *all* records published in the 'sxs' community, not just simulations
+                  '<id>': {  # This ID key is the same as the 'id' value inside this object
+                      # More details about this 'representation' object at http://developers.zenodo.org/#depositions
+                      'conceptdoi': '10.5281/zenodo.<conceptrecid>',  # Permanent DOI for all versions of this record
+                      'conceptrecid': '<conceptrecid>',  # ~7-digit integer identifying collectively all versions of this record
+                      'created': '<YYYY-MM-DDThh:mm:ss.ssssss>',  # UTC time of creation of this record on Zenodo
+                      'doi': '10.5281/zenodo.<id>',  # Permanent DOI for this record
+                      'doi_url': 'https://doi.org/10.5281/zenodo.<id>',  # URL for permanent DOI of this record
+                      'files': [
+                          # See https://data.black-holes.org/waveforms/documentation.html for
+                          # detailed descriptions of the *contents* of the files in each record.
+                          {
+                              'checksum': '<checksum>',  # MD5 checksum of file on Zenodo
+                              'filename': '<filename>',  # Name of file; may contain slashes denoting directories
+                              'filesize': <filesize>,  # Number of bytes in the file
+                              'id': '<fileid>',  # A standard UUID (hexadecimal with characters in the pattern 8-4-4-4-12)
+                              'links': {
+                                  'download': 'https://zenodo.org/api/files/<bucket>/<filename>',  # The URL to use to download this file
+                                  'self': 'https://zenodo.org/api/deposit/depositions/<deposition_id>/files/<fileid>'  # Ignore this
+                              }
+                          },
+                          ...  # Other file descriptions in the order in which they were uploaded (not necessarily a meaningful order)
+                      ]
+                      'id': <id>,  # ~7-digit integer uniquely identifying this record
+                      'links': {
+                           'badge': 'https://zenodo.org/badge/doi/10.5281/zenodo.<id>.svg',
+                           'bucket': 'https://zenodo.org/api/files/<uuid>',  # Base URL for file uploads and downloads
+                           'conceptbadge': 'https://zenodo.org/badge/doi/10.5281/zenodo.<conceptrecid>.svg',
+                           'conceptdoi': 'https://doi.org/10.5281/zenodo.<conceptrecid>',  # Permanent link to webpage for most-recent version
+                           'discard': 'https://zenodo.org/api/deposit/depositions/<id>/actions/discard',  # API action to discard a draft
+                           'doi': 'https://doi.org/10.5281/zenodo.<id>',  # Permanent URL for this version
+                           'edit': 'https://zenodo.org/api/deposit/depositions/<id>/actions/edit',  # API action to edit this record
+                           'files': 'https://zenodo.org/api/deposit/depositions/<id>/files',
+                           'html': 'https://zenodo.org/deposit/<id>',  # Webpage for this version
+                           'latest': 'https://zenodo.org/api/records/<id>',  # API endpoint for most-recent version
+                           'latest_html': 'https://zenodo.org/record/<id>',  # Webpage for most-recent version
+                           'publish': 'https://zenodo.org/api/deposit/depositions/<id>/actions/publish',
+                           'record': 'https://zenodo.org/api/records/<id>',
+                           'record_html': 'https://zenodo.org/record/<id>',  # Webpage for this particular version
+                           'self': 'https://zenodo.org/api/deposit/depositions/<id>'
+                      },
+                      'metadata': {  # Note that this is Zenodo metadata, and is different from the SXS metadata
+                          'access_right': '<access>',  # Can be 'open', 'closed', 'embargoed', or 'restricted'
+                          'communities': [
+                              {'identifier': '<community_name>'},  # Names may include 'sxs' and 'zenodo'
+                              ...
+                          ],
+                          'creators': [
+                              {
+                                  'name': '<name>',  # Name of this creator in the format Family name, Given names
+                                  'affiliation': '<affiliation>',  # Affiliation of this creator (optional)
+                                  'orcid': '<orcid>',  # ORCID identifier of this creator (optional)
+                                  'gnd': '<gnd>'  # GND identifier of this creator (optional)
+                              },
+                              ...
+                          ],
+                          'description': '<description>',  # Text description of this record
+                          'doi': '10.5281/zenodo.<id>',  # Permanent DOI of this record
+                          'keywords': [
+                              '<keyword>',  # Optional; this array may be empty
+                              ...
+                          ],
+                          'prereserve_doi': {'doi': '10.5281/zenodo.<id>', 'recid': <id>},
+                          'publication_date': '<YYYY-MM-DD>',  # Possibly meaningless date (UTC)
+                          'title': '<title>',
+                          'upload_type': 'dataset'
+                      },
+                      'modified': '<YYYY-MM-DDThh:mm:ss.ssssss>',  # (UTC) Last modification of this record (possibly just Zenodo metadata)
+                      'owner': <user_id>,  # ~5-digit integer identifying the user who owns this record
+                      'record_id': <id>,  # Same as 'id'
+                      'state': '<state>',  # Can be 'done', 'inprogress', 'error', 'unsubmitted', possibly others
+                      'submitted': <submitted>,  # True or false (always true for published records)
+                      'title': '<title>'  # Same as ['metadata']['title']
+                  },
+                  ...
+              },
+              'simulations': {
+                  '<sxs_id>': {  # The SXS ID is a string like SXS:BHNS:0001 or SXS:BBH:1234
+                      'conceptrecid': '<conceptrecid>',  # The Zenodo ID of the 'concept' record, which *resolves to* the most-recent version
+                      'versions': [  # Zenodo IDs of each version.  There may only be one; index this list with [-1] to always get the most recent
+                          '<id1>',  # Oldest version first
+                          ...
+                      ],
+                      'metadata': {
+                          # Variable content describing (mostly) physical parameters of the system.  It's basically a
+                          # python-compatible version of the information contained in 'metadata.txt' from the
+                          # highest-resolution run in the most-recent version of this simulation.  That file is meant to
+                          # be more-or-less as suggested in <https://arxiv.org/abs/0709.0093>.  The conversion to a
+                          # python-compatible format means that keys like 'simulation-name' have had hyphens replaced by
+                          # underscores so that they can be used as variable names in python and any other sane language
+                          # (with apologies to Lisp).  As far as possible, values that are just strings in that file
+                          # have been converted into the relevant types -- like numbers, integers, and arrays.  Note
+                          # that some keys like eccentricity are sometimes numbers and sometimes the string '<number'
+                          # (meaning that the eccentricity is less than the number), which is necessarily a string.
+                          #
+                          # Below are just the first few keys that *may* be present.  Note that closed-access
+                          # simulations will have empty dictionaries here.
+                          #
+                          'simulation_name': '<directory_name>',  # This may be distinctly uninformative
+                          'alternative_names': '<sxs_id>',  # This may be a list of strings
+                          'initial_data_type': '<type>',  # Something like 'BBH_CFMS'
+                          'number_of_orbits': <number>,  # This is a float
+                          'relaxed_mass1': <m2>,
+                          'relaxed_mass2': <m1>,
+                          'relaxed_dimensionless_spin1': [
+                              <chi1_x>,
+                              <chi1_y>,
+                              <chi1_z>
+                          ],
+                          'relaxed_dimensionless_spin2': [
+                              <chi2_x>,
+                              <chi2_y>,
+                              <chi2_z>
+                          ],
+                          'relaxed_eccentricity': <eccentricity>,  # Or maybe a string...
+                          'relaxed_orbital_frequency': [
+                              <omega_x>,
+                              <omega_y>,
+                              <omega_z>
+                          ],
+                          'relaxed_measurement_time': <time>,
+                          ...
+                      },
+                  },
+                  ...
+              }
+          }
+
+    'simulation_versions': {  # Map SXS ID (e.g., SXS:BBH:1234 or SXS:BHNS:0001) to list of versions of the Zenodo record
+                  '<sxs_id>': [
+                      '<id0>',  # Zenodo ID of the "concept" record, which encompasses all versions
+                      '<id1>',  # Zenodo ID of the oldest version; to get the newest, just index this list with [-1]
+                      ...
+                  ],
+                  ...
+              }
               '<sxs_id>': {  # The SXS ID is a string like SXS:BHNS:0001 or SXS:BBH:1234
                   'conceptrecid': '<conceptrecid>',  # The Zenodo ID of the 'concept' record, which resolves to the current version
                   'versions': [
-                      {  # Oldest version first (speficically, with respect to 'created' timestamp)
+                      {  # Oldest version first (specifically, with respect to 'created' timestamp)
                           'representation': {  # More details about this object at http://developers.zenodo.org/#depositions
                               'conceptdoi': '10.5281/zenodo.<conceptrecid>',  # Permanent DOI for all versions of this record
                               'conceptrecid': '<conceptrecid>',  # ~7-digit integer identifying collectively all versions of this record
@@ -45,7 +181,7 @@ catalog_file_description = """
                                    'files': 'https://zenodo.org/api/deposit/depositions/<id>/files',
                                    'html': 'https://zenodo.org/deposit/<id>',  # Webpage for this version
                                    'latest': 'https://zenodo.org/api/records/<id>',  # 
-                                   'latest_html': 'https://zenodo.org/record/<id>',  # Webpage for most recent version
+                                   'latest_html': 'https://zenodo.org/record/<id>',  # Webpage for most-recent version
                                    'publish': 'https://zenodo.org/api/deposit/depositions/<id>/actions/publish',
                                    'record': 'https://zenodo.org/api/records/<id>',
                                    'record_html': 'https://zenodo.org/record/<id>',
@@ -140,7 +276,7 @@ def compare_catalogs(c1, c2):
     # Remove keys that are allowed to differ
     for c in [c1, c2]:
         del c['catalog_file_description']
-        del c['last_file_modification']
+        del c['modified']
         for sxs_id in c:
             if 'versions' in c[sxs_id]:
                 for i in range(len(c[sxs_id]['versions'])):
@@ -173,7 +309,7 @@ def catalog_from_representation_list(representation_list):
             repr_by_sxs_id[sxs_id].append(r)
     catalog = OrderedDict()
     catalog['catalog_file_description'] = catalog_file_description
-    catalog['last_file_modification'] = sorted([r['modified'] for sxs_id in repr_by_sxs_id for r in repr_by_sxs_id[sxs_id]])[-1]
+    catalog['modified'] = sorted([r['modified'] for sxs_id in repr_by_sxs_id for r in repr_by_sxs_id[sxs_id]])[-1]
     for sxs_id in sorted(repr_by_sxs_id):
         catalog[sxs_id] = {}
         catalog[sxs_id]['conceptrecid'] = repr_by_sxs_id[sxs_id][0]['conceptrecid']
@@ -182,6 +318,16 @@ def catalog_from_representation_list(representation_list):
             for r in sorted(repr_by_sxs_id[sxs_id], key=lambda r: r['created'])
         ]
     return catalog
+
+
+def update(catalog_file_name='catalog.json'):
+    import json
+    from . import Login
+    with open(catalog_file_name, 'r') as f:
+        catalog = json.load(f)
+    modified = catalog['modified']
+    l = Login()
+    new_records = l.list_deposits(q='updated:["{0}" TO *]'.format(modified), status='published', all_versions=True)
 
 
 def map(catalog_file_name='complete_catalog.json', map_file_name='sxs_to_zenodo.map'):
@@ -229,7 +375,7 @@ def map(catalog_file_name='complete_catalog.json', map_file_name='sxs_to_zenodo.
         f.write("}\n")
 
 
-def update(complete_catalog_file_name='complete_catalog.json', public_catalog_file_name='public_catalog.json', *args, **kwargs):
+def catalog(complete_catalog_file_name='complete_catalog.json', public_catalog_file_name='public_catalog.json', *args, **kwargs):
     """Update (or construct) a catalog of SXS simulations hosted by Zenodo
 
     NOTE: This function returns two catalogs: one "complete" catalog containing all records
