af13b1164520809f9bcd2839de10db4be01f465a
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-05-08 15:59:37 -0400

    Add function for computing SXS ID

--- sxs/__init__.py
+++ sxs/__init__.py
@@ -11,3 +11,21 @@ import sxs.references
 import sxs.zenodo
 
 sxs_identifier_regex = r'(?P<sxs_identifier>SXS:(?P<simulation_type>BBH|BHNS|NSNS):(?P<sxs_number>[0-9]*))'
+
+def sxs_id(s):
+    """Return the SXS ID contained in the input string
+
+    An SXS ID is anything that matches the following regular expression:
+
+        SXS:(BBH|BHNS|NSNS):[0-9]*
+
+    If no match is found, the empty string is returned.  If multiple matches are found, only the
+    first is returned.
+
+    """
+    import re
+    m = re.search(sxs_identifier_regex, s)
+    if m:
+        return m['sxs_identifier']
+    else:
+        return ''
