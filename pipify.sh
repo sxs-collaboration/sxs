6cbd2e6939fea5c7dfc49e9d3186d877d8cb78e3
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-12-15 21:54:02 +1300

    Update pip upload

--- pipify.sh
+++ pipify.sh
@@ -1,2 +1,9 @@
-/bin/rm -rf build sxs.egg-info
-python setup.py bdist_wheel --universal upload
+/bin/rm -rf build dist sxs.egg-info
+
+# # This is deprecated
+# python setup.py bdist_wheel --universal upload
+
+# This is the new way
+pip install --quiet --upgrade twine
+python setup.py sdist bdist_wheel --universal
+twine upload dist/*
