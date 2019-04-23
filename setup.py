512853300482ba97d7ffc78aaea55a9978044277
Michael Boyle <michael.oliver.boyle@gmail.com>
2019-04-22 21:04:23 -0400

    Correct import of subsubmodule

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     from setuptools import setup
     setup(name='sxs',
           packages = ['sxs', 'sxs.metadata', 'sxs.doxygen', 'sxs.references',
-                      'sxs.validate', 'sxs.zenodo', 'sxs.zenodo.api'],
+                      'sxs.validate', 'sxs.validate.bbh', 'sxs.zenodo', 'sxs.zenodo.api'],
           scripts = ['scripts/sxs'],
           include_package_data=True,
           version=version,
