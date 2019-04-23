56c3945e1243052b005602a7dddf3ef42cfe80d0
Michael Boyle <michael.oliver.boyle@gmail.com>
2019-04-22 20:54:24 -0400

    Make `sxs_id` function more flexible

--- sxs/__init__.py
+++ sxs/__init__.py
@@ -24,8 +24,31 @@ def sxs_id(s):
     If no match is found, the empty string is returned.  If multiple matches are found, only the
     first is returned.
 
+    The input parameter may be
+    1) A string believed to contain the SXS ID
+    2) The path to a file (like common-metadata.txt) containing such a string
+    3) An object with a 'title' attribute
+    4) An object with a 'title' item
+
     """
+    import os.path
     import re
+    try:
+        s = s['title']
+    except TypeError:
+        pass
+    try:
+        s = s.title
+    except AttributeError:
+        pass
+    if os.path.isfile(s):
+        with open(s, 'r') as f:
+            s = [l.strip() for l in f.splitlines()]
+        for line in s:
+            sxs_id_line = sxs_id(line)
+            if sxs_id_line:
+                return sxs_id_line
+        return ''
     m = re.search(sxs_identifier_regex, s)
     if m:
         return m['sxs_identifier']
