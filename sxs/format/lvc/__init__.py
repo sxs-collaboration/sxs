ea8d1c860e6d85d0bff111694af2e63fa281acd0
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-01-14 15:12:47 -0500

    Remove redundant information from format.lvc.convert_simulation

--- sxs/format/lvc/__init__.py
+++ sxs/format/lvc/__init__.py
@@ -15,40 +15,25 @@ def bbh_keys_from_simulation_keys(simulation_keys):
             if simulation_key.split(':')[-2] == "BBH"]
 
 
-def convert_simulation(sxs_data_path, resolution, sxs_catalog_metadata_path,
-                       sxs_catalog_resolutions_path, out_path, modes=8,
+def convert_simulation(sxs_data_path, out_path,
+                       sxs_catalog_path='~/.sxs/catalog', resolution=None, modes=8,
                        truncation_time=None):
     """Convert a simulation from the SXS BBH catalog into the LVC format.
     
-    sxs_data_path is a path to a directory that must contain the following:
-        i)   rhOverM_Asymptotic_GeometricUnits_CoM.h5
-        ii)  Horizons.h5
-        iii) metadata.json
-    Additionally, the function requires paths to the following:
-        iv)  sxs_catalog_metadata_path points to sxs_catalog.json, which
-             contains information such as masses and spins. A file in this
-             format is available at https://data.black-holes.org/catalog.json.
-        v)   sxs_catalog_resolutions_path points to a file whose keys are
-             SXS ID numbers and whose values are lists of integers, where each
-             integer corresponds to a resolution available in the catalog.
-    The option resolution is an integer labeling the resolution of the
-    converted waveform. Modes is an array of the format
-    [[l1, m1], [l2, m2], ...] listing the l,m modes to convert. This function
-    outputs a file in LVC format named SXS_BBH_####_Res#.h5
-    in out_path.
+    This function outputs a file in LVC format named SXS_BBH_####_Res#.h5 in out_path.
 
     Parameters
     ==========
     sxs_data_path: string
-        Path containing rh*h5, Horizons.h5, metadata.json
-    resolution: int
-        Integer giving the resolution of the data to convert
-    sxs_catalog_metadata_path: string
-        Path to sxs_catalog.json (via get_sxs_public_metadata.py)
-    sxs_catalog_resolutions_path: string
-        Path to sxs_catalog_resolutions.json
+        Path to directory containing rhOverM_Asymptotic_GeometricUnits_CoM.h5, Horizons.h5,
+        and metadata.json files.
     out_path: string
         Path where LVC format file is to be output
+    sxs_catalog_path: string [defaults to '~/.sxs/catalog']
+        Path passed to `sxs.zenodo.catalog.read`.  See that function's docstring for details.
+    resolution: int or None [defaults to None]
+        Integer giving the resolution (Lev) of the data to convert.  If this is not given,
+        the resolution is determined automatically from sxs_data_path.
     modes: '22only' or int for max l [defaults to 8]
         Modes to be placed in the output file.  Passing '22only' results
         in the (2,2) and (2,-2) modes being output.  Otherwise, each
@@ -63,6 +48,7 @@ def convert_simulation(sxs_data_path, resolution, sxs_catalog_metadata_path,
     import json
     import os
 
+    from ... import lev_number, zenodo
     from .metadata import sxs_id_from_alt_names, write_metadata_from_sxs
     from .horizons import horizon_splines_from_sxs, write_horizon_splines_from_sxs
     from .waveforms import spline_amp_phase_from_sxs, write_splines_to_H5
@@ -104,23 +90,31 @@ def convert_simulation(sxs_data_path, resolution, sxs_catalog_metadata_path,
 
     log = Log()
 
-    with open(sxs_data_path + "/metadata.json", 'r') as f:
+    # Load metadata.json from this simulation
+    with open(os.path.join(sxs_data_path, "metadata.json"), 'r') as f:
         metadata = json.load(f)
-    with open(sxs_catalog_metadata_path, 'r') as f:
-        sxs_catalog_metadata = json.load(f)
-    with open(sxs_catalog_resolutions_path, 'r') as f:
-        sxs_catalog_resolutions = json.load(f)
+
+    # Load catalog metadata
+    if sxs_catalog_path is None:
+        sxs_catalog = zenodo.catalog.read()
+    else:
+        sxs_catalog = zenodo.catalog.read(sxs_catalog_path)
+
+    # Determine the resolution of the input simulation, if needed
+    if resolution is None:
+        resolution = lev_number(sxs_data_path)
+
+    sxs_catalog_resolutions = zenodo.catalog.resolutions_for_simulations(sxs_catalog)
 
     sxs_id = sxs_id_from_alt_names(metadata['alternative_names'])
     log("Converting " + sxs_id)
-    log("Running script " + os.path.realpath(__file__) + " in " + os.getcwd())
-    log("convert_sxs_to_lvc.py called with the following parameters:")
+    log("sxs.format.lvc.convert_simulation( called with the following parameters:")
     log("  sxs_data_path: " + sxs_data_path)
-    log("  resolution: " + str(resolution))
-    log("  sxs_catalog_metadata_path: " + sxs_catalog_metadata_path)
-    log("  sxs_catalog_resolutions_path: " + sxs_catalog_resolutions_path)
-    log("  modes: " + str(modes))
     log("  out_path: " + str(out_path))
+    log("  sxs_catalog_path: " + sxs_catalog_path)
+    log("  resolution: " + str(resolution))
+    log("  modes: " + str(modes))
+    log("  truncation_time: " + str(truncation_time))
 
     extrapolation_order = "Extrapolated_N2"
     log("Extrapolation order: " + extrapolation_order)
@@ -141,7 +135,7 @@ def convert_simulation(sxs_data_path, resolution, sxs_catalog_metadata_path,
     write_horizon_splines_from_sxs(out_name, horizon_splines_to_write, t_A, t_B, t_C, log)
 
     write_metadata_from_sxs(out_name, resolution, metadata,
-                            sxs_catalog_metadata, sxs_catalog_resolutions,
+                            sxs_catalog, sxs_catalog_resolutions,
                             start_time, peak_time, l_max, log)
 
     with h5py.File(out_name, 'a') as out_file:
