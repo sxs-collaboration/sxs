657582333651138853dcdd98e89490201eed707b
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-01-28 10:23:22 -0500

    Switch to classmethod reader

--- sxs/format/__init__.py
+++ sxs/format/__init__.py
@@ -3,24 +3,32 @@ from . import lvc
 
 
 class Waveform(object):
-    def __init__(self, file_path, group_path='/', modes=None, ell_min=2, ell_max=8):
+    """Contains a waveform in SXS"""
+    def __init__(self, *args, **kwargs):
+        if args or kwargs:
+            raise ValueError("This is an empty constructor; use `from_data` or `read` to add data.")
+
+    @classmethod
+    def read(cls, file_path, group_path='/', modes=None, ell_min=2, ell_max=8):
         import numpy as np
         import h5py
+        w = cls()
         modes_string = "Y_l{0[0]}_m{0[1]}.dat"
         modes = modes or [[ell, m] for ell in range(ell_min, ell_max+1) for m in range(-ell, ell+1)]
         with h5py.File(file_path, 'r') as f:
             h = f[group_path]
-            self.t = h[modes_string.format(modes[0])][:, 0]
-            self.t.setflags(write=False)
-            self.data = np.empty((self.t.size, len(modes)*2))
+            w.t = h[modes_string.format(modes[0])][:, 0]
+            w.t.setflags(write=False)
+            w.data = np.empty((w.t.size, len(modes)*2))
             for i, mode in enumerate(modes):
-                self.data[:, 2*i:2*i+2] = h[modes_string.format(mode)][:, 1:3]
-            self.data = self.data.view(complex)
-            self.data.setflags(write=False)
+                w.data[:, 2*i:2*i+2] = h[modes_string.format(mode)][:, 1:3]
+            w.data = w.data.view(complex)
+            w.data.setflags(write=False)
 
-            self.version_hist = f.get('VersionHist.ver', None)
-            if self.version_hist is not None:
-                self.version_hist = self.version_hist[:]
+            w.version_hist = f.get('VersionHist.ver', None)
+            if w.version_hist is not None:
+                w.version_hist = w.version_hist[:]
+        return w
 
     def peak_index(self, start_index=0):
         import numpy as np
