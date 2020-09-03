d9d0add9d872f692c00d01ea9f844992d0d853c4
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-03 15:58:41 -0400

    Move default_shuffle_widths to more appropriate utilities package

--- sxs/data/__init__.py
+++ sxs/data/__init__.py
@@ -7,10 +7,6 @@ from .waveforms import WaveformModes, WaveformGrid, WaveformSignal
 from . import time_series, metadata, horizons, waveforms
 
 
-# This is a generically reliable set of widths to feed to multishuffle when using 64-bit floats
-default_shuffle_widths = (8, 8, 4, 4, 4, 2,) + (1,) * 34
-
-
 def load(sxs_id, version='latest', data='h', lev='+', extrapolation='N4', inertial=True, **kwargs):
     """
 
