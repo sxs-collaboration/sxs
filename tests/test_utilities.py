bb372be0d6fc1cf42e3346c9f701ac3a5c5ffef0
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-06 14:18:35 -0400

    Use pytest monkeypatch to set environment variables temporarily

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,11 +2,8 @@ import pytest
 
 import sxs
 
-from .conftest import environment_context
-
-
 @pytest.mark.parametrize("persistent", (True, False))
-def test_sxs_directory(persistent):
+def test_sxs_directory_bad_directory_name(persistent):
     with pytest.raises(ValueError):
         sxs.utilities.sxs_directory("cacheconfig", persistent=persistent)
 
@@ -21,11 +18,14 @@ def test_sxs_directory(persistent):
     ),
 )
 @pytest.mark.forked
-def test_sxs_directory_cache_env(directory_type, persistent, tmp_path):
+def test_sxs_directory_cache_env(directory_type, persistent, tmp_path, monkeypatch):
     import pathlib
     import os
     sxs.utilities.sxs_directory.cache_clear()
-    with environment_context(**{f"SXS{directory_type.upper()}DIR": str(tmp_path)}):
+    # with environment_context(**{f"SXS{directory_type.upper()}DIR": str(tmp_path)}):
+    with monkeypatch.context() as mp:
+        mp.setattr(pathlib.Path, "home", lambda: tmp_path)
+        mp.setenv(f"SXS{directory_type.upper()}DIR", str(tmp_path))
         sxs_dir = sxs.utilities.sxs_directory(directory_type, persistent=persistent)
     assert isinstance(sxs_dir, pathlib.Path)
     if persistent:
@@ -50,17 +50,23 @@ def test_sxs_directory_cache_env(directory_type, persistent, tmp_path):
     ),
 )
 @pytest.mark.forked
-def test_sxs_directory_unwritable(directory_type, tmp_path):
+def test_sxs_directory_unwritable(directory_type, tmp_path, monkeypatch):
     import time
     import os
     import stat
+    from pathlib import Path
+
     d = tmp_path / "unwritable"
     d.mkdir(mode=0o000)
     assert d.exists()
     assert d.is_dir()
     assert stat.filemode(d.stat().st_mode) == "d---------"
     assert not os.access(str(d), os.W_OK)
-    with environment_context(**{"HOME": str(d), f"SXS{directory_type.upper()}DIR": str(d)}):
+
+    # with environment_context(**{"HOME": str(d), f"SXS{directory_type.upper()}DIR": str(d)}):
+    with monkeypatch.context() as mp:
+        mp.setattr(Path, "home", lambda: d)
+        mp.setenv(f"SXS{directory_type.upper()}DIR", str(d))
         sxs.utilities.sxs_directory.cache_clear()
         with pytest.warns(UserWarning):
             sxs_dir = sxs.utilities.sxs_directory(directory_type, persistent=True)
@@ -71,11 +77,16 @@ def test_sxs_directory_unwritable(directory_type, tmp_path):
     time.sleep(0.1)
 
 
-def test_read_write_config(tmp_path):
+def test_read_write_config(tmp_path, monkeypatch):
+    import pathlib
+
     original_config = sxs.utilities.read_config()
 
     sxs.utilities.sxs_directory.cache_clear()
-    with environment_context(**{f"SXSCONFIGDIR": str(tmp_path)}):
+    # with environment_context(**{f"SXSCONFIGDIR": str(tmp_path)}):
+    with monkeypatch.context() as mp:
+        mp.setattr(pathlib.Path, "home", lambda: tmp_path)
+        mp.setenv(f"SXSCONFIGDIR", str(tmp_path))
         assert not sxs.utilities.read_config()
         sxs.utilities.write_config(NONSENSEGARBAGE=123)
         read = sxs.utilities.read_config()
