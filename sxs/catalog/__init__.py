6e7c880710ec5b290acc8742a40134b5623db050
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-09 13:13:59 -0400

    Make Catalog.select a simple wrapper for utilities.select_from...

--- sxs/catalog/__init__.py
+++ sxs/catalog/__init__.py
@@ -17,66 +17,67 @@ class Catalog(object):
     def __init__(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def select(self, location, /, *args, **kwargs):
-        import re
-        # construct a map of keys in SXS IDs (with versions) and values of
-        #     maps of keys in levs and values of
-        #         maps of keys in file names and values of
-        #             URLs
+    def select(self, path_pattern):
+        """Select from all catalog files by progressively matching path components
 
-        # Split input string by '/'
-        # i = 0
-        # j = 0
-        # for j in range(4)
-        #   search map_j for split
-        #   if exact match i += 1 and select match
-        #   if partial match i += 1 and select highest match
-        #   if regex match i += 1 and select all matches
-        #   if not select highest key
+        Parameters
+        ----------
+        path_pattern : str
+            A pattern to search for among the catalog files.  This is first searched as
+            a literal substring, which will return a set of one or more matches.  If no
+            matches were found, the pattern is split into components by "/", which are
+            used progressively to match corresponding path components — first as
+            literal substrings, then as (python-style) regular expressions.  Each
+            partial match in this step will pass all the matched components to the
+            `max` function, and choose only the result — though with regexes, there may
+            be distinct partial matches, each of which will pass only its corresponding
+            components to `max`, resulting in multiple matches.
 
-        # "SXS:BBH:0123v4/Lev4/h_Extrapolated_N2.h5"
-        # "SXS:BBH:0123v1/Lev4/rhOverM_Asymptotic_GeometricUnits_CoM.h5/Extrapolated_N2.dir"
+        Returns
+        -------
+        matched_paths : Set[str]
 
-        # return tuple of 3-tuples (full_match_string, h5group, URL)
-        raise NotImplementedError()
+        See Also
+        --------
+        sxs.utilities.select_by_path_component
 
-        parts = location.split("/")
-        if parts[0].lower().startswith(("catalog", "sxs")) and "v" in parts[0]:
-            # The user has asked for a specific version
-            files = self.files_with_all_versions
-        else:
-            files = self.files_with_latest_version
+        Examples
+        --------
+        First, we can choose the `h` waveform with `n=2` extrapolation in the
+        highest-resolution (Lev) run from the simulation SXS:BBH:0002 with
 
-        matches = [""]
-        part_index = 0
-        for matching_index in range(len(file_parts)):
-            if part_index >= len(parts):
-                break
-            part = parts[part_index]
-            files_index = files[matching_index]
-            if not part:
-                matches = [sorted(files_index)[-1]]
-                part_index += 1
-            else:
-                part_lower = part.lower()
-                matches = [f for f in files_index if f.lower().startswith(part_lower)]
-                if matches:
-                    # We have matches of the raw string, so we just take the "largest"
-                    matches = [sorted(matches)[-1]]
-                else:
-                    # We'll try to compile this part as a regex, and take *everything* that matches.
-                    # Note that re.Pattern.match requires a match at the *beginning* of the string.
-                    part = re.compile(part, re.IGNORECASE)
-                    matches = [f for f in files_index if part.match(f)]
-                if matches:
-                    part_index += 1
-                else:
-                    matches = [sorted(files_index)[-1]]
+            >>> catalog.select("SXS:BBH:0002/Lev/h_ex.*_n2")
+            {"SXS:BBH:0002v7/Lev6/h_extrapolated_n2.h5"}
 
-    @property
-    @functools.lru_cache()
-    def nested_maps(self):
-        raise NotImplementedError()
+        Because the "Lev" component of the input string only matched the "Lev" portion
+        of the path components "Lev4", "Lev5", and "Lev6", all of those components were
+        passed to `max` together, and only "Lev6" was returned.  Similarly, the highest
+        version of SXS:BBH:0002 (currently, v7) was chosen automatically.
+
+        We could, instead, use a regular expression to include the numbers in the
+        matches:
+
+            >>> catalog.select("SXS:BBH:0002/Lev./h_ex.*_n2")
+            {"SXS:BBH:0002v7/Lev4/h_extrapolated_n2.h5",
+             "SXS:BBH:0002v7/Lev5/h_extrapolated_n2.h5",
+             "SXS:BBH:0002v7/Lev6/h_extrapolated_n2.h5"}
+
+        In this case, the pattern "Lev." matches each component entirely, so they are
+        each retained.  Similarly, "SXS:BBH:0002v." would match all versions, and
+        "h_ex.*_n." would match all extrapolation orders — and "h.*" would match all
+        extrapolation orders, as well as the outermost extraction.
+
+        Another helpful pattern is to use alternation in the regular expression, to
+        explicitly list acceptable matches:
+
+            >>> catalog.select("SXS:BBH:0002/Lev(4|6)/h_ex.*_n2")
+            {"SXS:BBH:0002v7/Lev4/h_extrapolated_n2.h5",
+             "SXS:BBH:0002v7/Lev6/h_extrapolated_n2.h5"}
+
+        """
+        from ..utilities import select_by_path_component
+        files = self.files
+        selections = select_by_path_component(location, set(self.files))
 
     @property
     def description(self):
