c8117986dc1892b40531b1d41eddbe25a243e826
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-11 00:17:03 -0400

    Tweak docstrings

--- sxs/zenodo/surrogatemodeling.py
+++ sxs/zenodo/surrogatemodeling.py
@@ -2,17 +2,19 @@ def sync(annex_dir='./', exclude=[], publish='if_pending',
          sandbox=False, access_token_path=None, skip_checksums='if_file_is_older'):
     """Sync the SurrogateModeling annex to Zenodo
 
-    This function first searches for any changes in the data files; if there are any, a new version
-    is created on Zenodo, and the relevant files are uploaded, deleted, etc.  It then collects the
-    Zenodo metadata (author list, related identifiers, etc.) from zenodo_metadata.json, along with
-    the description to be placed on the Zenodo record from zenodo_description.html.  These changes
-    are made to the Zenodo record, whether the existing record (if no files changed) or the new
-    record (if files changed).
+    This function first searches for any changes in the data files; if there are
+    any, a new version is created on Zenodo, and the relevant files are uploaded,
+    deleted, etc.  It then collects the Zenodo metadata (author list, related
+    identifiers, etc.) from zenodo_metadata.json, along with the description to be
+    placed on the Zenodo record from zenodo_description.html.  These changes are
+    made to the Zenodo record, whether the existing record (if no files changed) or
+    the new record (if files changed).
 
     Parameters
     ----------
     annex_dir : str, optional [defaults to './']
-        Absolute or relative path to base directory of SurrogateModeling annex with data files.
+        Absolute or relative path to base directory of SurrogateModeling annex with
+        data files.
 
     """
     import os
