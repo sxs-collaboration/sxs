de40b708a59181775ce482eef370abca1b83ef3d
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-14 13:49:14 -0400

    Move some basic methods to the core mixin

--- sxs/waveforms/waveform_mixin.py
+++ sxs/waveforms/waveform_mixin.py
@@ -2,10 +2,33 @@ import abc
 
 
 class WaveformMixin(abc.ABC):
+    # Note: This is currently a pretty trivial class, but as the code develops
+    # this will be increasingly useful.
+
     @property
     @abc.abstractmethod
-    def data(self):
+    def t(self):  # Handy alias for backwards compatibility
+        return self.time
+
+    @property
+    @abc.abstractmethod
+    def data(self):  # Handy alias for backwards compatibility
         return self.ndarray
+
+    @property
+    @abc.abstractmethod
+    def data_type(self):
+        return self._metadata.get("data_type", "unknown")
+
+    @property
+    @abc.abstractmethod
+    def frame(self):
+        return self._metadata.get("frame", np.atleast_2d(quaternionic.one))
+
+    @property
+    @abc.abstractmethod
+    def frame_type(self):
+        return self._metadata.get("frame_type", "unknown")
 
     # metadata
     # spin_weight
@@ -15,6 +38,6 @@ class WaveformMixin(abc.ABC):
     #     radius_scaling
     #     mass_scaling
     #     conformal_scaling
-    #     frame ['inertial', 'corotating', ...]
+    #     frame ∈ {'inertial', 'corotating', ...}
     # copy()
     # evaluate(units, total_mass, theta, phi, orientation, time, ...)  # Return WaveformSignal
