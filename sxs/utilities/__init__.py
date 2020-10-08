87f4dbadec5a856e499e260f807da119362ff041
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-10-08 10:23:28 -0400

    Pre-compile SXS ID regexes

--- sxs/utilities/__init__.py
+++ sxs/utilities/__init__.py
@@ -13,7 +13,9 @@ default_shuffle_widths = (8, 8, 4, 4, 4, 2,) + (1,) * 34
 from . import url, inspire, monotonicity, decimation, lvcnr
 from .downloads import download_file
 from .bitwise import xor, multishuffle
-from .sxs_identifiers import sxs_identifier_regex, lev_regex, sxs_id, lev_number, simulation_title
+from .sxs_identifiers import (
+    sxs_identifier_regex, sxs_identifier_re, lev_regex, lev_re, sxs_id, lev_number, simulation_title
+)
 from .sxs_directories import sxs_directory, read_config, write_config, sxs_path_to_system_path, cached_path
 from .select import select_by_path_component
 from .formats import file_format
