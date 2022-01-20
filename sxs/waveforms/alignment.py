Fixing normalization bug due to sxs/scri difference

sxs/waveforms/alignment.py
@@ -241,7 +241,7 @@ def align2d(wa, wb, t1, t2, n_brute_force_δt=None, n_brute_force_δϕ=5, includ
     modes_A = CubicSpline(wa.t, wa[:,LM(2, -2, wa.ell_min):LM(ell_max+1, -(ell_max+1), wa.ell_min)].data)
     modes_B = CubicSpline(wb.t, wb[:,LM(2, -2, wb.ell_min):LM(ell_max+1, -(ell_max+1), wb.ell_min)].data)(t_reference)
     
-    normalization = trapezoid(CubicSpline(wb.t, wb[:,2:ell_max + 1].norm)(t_reference), t_reference)
+    normalization = trapezoid(CubicSpline(wb.t, wb[:,LM(2, -2, wb.ell_min):LM(ell_max+1, -(ell_max+1), wb.ell_min)].norm)(t_reference), t_reference)
     
     δϕ_factor = np.array([M for L in range(wa.ell_min, wa.ell_max + 1) for M in range(-L, L + 1)])
     def cost(δt_δϕ):