37557d6f091df474a93441859682c783316fda1a
user.email <michael.oliver.boyle@gmail.com>
2022-08-19 16:18:38 -0400

    Use DataCite schema

--- sxs/caltechdata/__init__.py
+++ sxs/caltechdata/__init__.py
@@ -6,18 +6,23 @@ from .api import Login, Deposit, Records
 import datetime
 import json
 import pathlib
+from caltechdata_api import customize_schema
 from .. import sxs_id, Metadata
 
 
-"""Update/upload data
+def upload_simannex_dir(login, directory, date=None):
+    """Update/upload data from the SimAnnex to CaltechDATA
 
+    Parameters
+    ----------
+    login : caltechdata.Login
+        This is used to communicate with CaltechDATA.
+    directory : str or pathlib.Path
+        This must contain either "Public" or "Private", followed by the specific
+        directory in SimAnnex to upload.  It can also contain higher path elements
+        — for example, you can give the absolute path.
 
-"""
-
-
-def upload(directory, login=None):
-    if login is None:
-        login = Login()
+    """
 
     allowed_files = [
         "metadata.json",
@@ -73,91 +78,66 @@ def upload(directory, login=None):
         raise ValueError(f"""Could not find SXS ID in {directory / "common-metadata.txt"}""")
 
     # Find our list of files
-    files = [f.relative_to(directory) for f in directory.glob("Lev*/*") if f.name in allowed_files]
-    highest_lev = sorted(set(f.parent for f in files))[-1]
+    files = [f for f in directory.glob("Lev*/*") if f.name in allowed_files]
+    highest_lev = sorted(set(f.relative_to(directory).parent for f in files))[-1]
 
-    # Set up the metadata; see https://inveniordm.docs.cern.ch/reference/metadata for details
-    resource_type = {
-        "id": "dataset"
-    }  # https://github.com/caltechlibrary/caltechdata/blob/main/app_data/vocabularies/resource_types.yaml
-    creators = [
-        {
-            "person_or_org": {
-                "type": "organizational",
-                "name": "SXS Collaboration",
+    # Set up the metadata.  Note that CaltechDATA internally uses an undocumented
+    # customized schema similar to — but different from — the standard Invenio
+    # schema.  Fortunately, `caltechdata_api` includes a function to convert from
+    # DataCite 4.0 or 4.3 to their schema.  So, we set ours up as DataCite 4.0 and
+    # convert it: <https://schema.datacite.org/meta/kernel-4.0/>
+    metadata = customize_schema({
+        "resourceType": {
+            "resourceTypeGeneral": "Dataset"
+        },
+        "titles": [
+            {"title": f"Binary black-hole simulation {sxsid}"},
+            {"title": f"{original_name}", "titleType": "AlternativeTitle"},
+        ],
+        "version": "10",
+        "creators": [{"creatorName": "SXS Collaboration"}],
+        "descriptions": [
+            {
+                "description": 'Simulation of a black-hole binary system evolved by the <a href="https://www.black-holes.org/code/SpEC.html">SpEC code</a>.',
+                "descriptionType": "Abstract"
+            }
+        ],
+        "subjects": [
+            {"subject": "Astronomy"},
+            {"subject": "Astrophysics"},
+            {"subject": "Gravitational Waves"},
+            {"subject": "Numerical Relativity"},
+            {"subject": "Black Holes"},
+            {"subject": "Neutron Stars"},
+        ],
+        "dates": [
+            {
+                "date": date or datetime.datetime.utcnow().strftime("%Y-%m-%d"),
+                "dateType": "Created"
             },
-        }
-    ]
-    title = f"Binary black-hole simulation {sxsid}"
-    publication_date = datetime.datetime.utcnow().strftime("%Y-%M-%d")
-    additional_titles = [
-        {
-            "title": f"{original_name}",
-            "type": {
-                "id": "alternative-title",
-                "title": {"en": "Original simulation name"},
+            {
+                "date": datetime.datetime.utcnow().strftime("%Y-%m-%d"),
+                "dateType": "Updated"
             },
-        }
-    ]
-    description = 'Simulation of a black-hole binary system evolved by the <a href="https://www.black-holes.org/code/SpEC.html">SpEC code</a>.'
-    rights = [{"id": "cc-by-4.0"}]
-    subjects = [
-        {"id": "http://www.oecd.org/science/inno/38235147.pdf?1.3"},
-        {"subject": "astronomy"},
-        {"subject": "astrophysics"},
-        {"subject": "gravitational waves"},
-        {"subject": "numerical relativity"},
-        {"subject": "black holes"},
-    ]
-    version = "v10"
-    default_preview_file = str(highest_lev / "metadata.json")
+        ],
+        "rightsList": [
+            {
+                "rights": "Creative Commons Attribution 4.0 International License",
+                "rightsURI": "https://creativecommons.org/licenses/by/4.0/",
+                "rightsIdentifier": "CC-BY-4.0",
+            }
+        ],
+    }, schema="40")
 
-    metadata = {
-        "access": {"files": "public", "record": "public"},
-        "metadata": {
-            "resource_type": resource_type,
-            "creators": creators,
-            "title": title,
-            "publication_date": publication_date,
-            "additional_titles": additional_titles,
-            "description": description,
-            "rights": rights,
-            "subjects": subjects,
-            "version": version,
-        },
-        "files": {
-            "enabled": True,
-            "default_preview": default_preview_file,
-        },
-    }
-
-    metadata = {
-        "access": {"record": "public", "files": "public"},
-        "files": {"enabled": True},
-        "metadata": {
-            "creators": [
-                {"person_or_org": {"family_name": "Brown", "given_name": "Troy", "type": "personal"}},
-                {
-                    "person_or_org": {
-                        "family_name": "Collins",
-                        "given_name": "Thomas",
-                        "identifiers": [{"scheme": "orcid", "identifier": "0000-0002-1825-0097"}],
-                        "name": "Collins, Thomas",
-                        "type": "personal",
-                    },
-                    "affiliations": [{"id": "01ggx4157", "name": "Entity One"}],
-                },
-            ],
-            "publication_date": "2020-06-01",
-            "resource_type": {"id": "image-photo"},
-            "title": "A Romans story",
-        },
-    }
+    recordurl = f"{login.base_url}submit/api/create/"
+    s3surl = f"{login.base_url}tindfiles/sign_s3/"
+    chkurl = f"{login.base_url}tindfiles/md5_s3"
 
     # Search for an existing record with this name
     existing = login.search(f'title:"{sxsid}"')
+    exists = existing.get("hits", {}).get("total", 0) > 0
 
-    if existing.get("hits", {}).get("total", 0) > 0:
+    if exists:
         raise NotImplementedError()
         # find the latest version
         # update `metadata` with its content, and bump `publication_date` and `version`
@@ -165,20 +145,21 @@ def upload(directory, login=None):
         # if files have changed, edit the existing record and proceed
         # otherwise, exit early
     else:
+        pass
         # create a new record and proceed
         # d = login.deposit(deposition_id=None)
 
-        url = f"{login.base_url}api/records"
-        r = login.session.post(url, data=json.dumps(metadata), headers={"Content-Type": "application/json"})
-        if r.status_code != 201:
-            print(f"Unable to create a new deposit on {url}.")
-            try:
-                print(r.json())
-            except:
-                pass
-            r.raise_for_status()
-            raise RuntimeError()  # Will only happen if the response was not strictly an error
-        links = r.json()["links"]
+        # url = f"{login.base_url}api/records"
+        # r = login.session.post(url, data=json.dumps(metadata))
+        # if r.status_code != 201:
+        #     print(f"Unable to create a new deposit on {url}.")
+        #     try:
+        #         print(r.json())
+        #     except:
+        #         pass
+        #     r.raise_for_status()
+        #     raise RuntimeError()  # Will only happen if the response was not strictly an error
+        # links = r.json()["links"]
         # url = "{0}api/records".format(login.base_url)
         # r = login.session.get(url, data=json.dumps(metadata))
         # assert r.status_code == 201, \
@@ -188,24 +169,9 @@ def upload(directory, login=None):
     # links = r.json()["links"]
 
     # Upload files
-    for f in files:
-        name = f.relative_to(directory)
+    metadata["files"] = [login.send_s3(f, f.relative_to(directory), verbose=True) for f in files]
 
-        # Initiate the file
-        r = login.session.post(links["files"], data=json.dumps([{"key": name}]))
-        assert r.status_code == 201, f"Failed to create file {f} (code: {r.status_code})"
-        file_links = r.json()["entries"][0]["links"]
+    # Now tell CaltechDATA about it
+    response = login.session._post(recordurl, data=json.dumps({"record": metadata}))
 
-        # Upload file content by streaming the data
-        with open(f, "rb") as fp:
-            r = login.session.put(file_links["content"], data=fp, headers={"Content-Type": "application/octet-stream"})
-        assert r.status_code == 200, f"Failed to upload file contet {f} (code: {r.status_code})"
-
-        # Commit the file.
-        r = login.session.post(file_links["commit"])
-        assert r.status_code == 200, f"Failed to commit file {f} (code: {r.status_code})"
-
-    # Publish
-    d.publish()
-
-    return d
+    return response.text
