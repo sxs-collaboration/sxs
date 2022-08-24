b6e877419220e387992aa0abaff21f7bc4c7fce6
user.email <michael.oliver.boyle@gmail.com>
2022-08-24 14:53:50 -0400

    Add SimpleVersion class

--- sxs/utilities/__init__.py
+++ sxs/utilities/__init__.py
@@ -52,3 +52,65 @@ def version_info():
         except Exception:
             pass
     return info
+
+
+class SimpleVersion:
+    """Basic versioning class
+
+    This class uses a simple two-element version string, enabling printing,
+    incrementing, and comparison.
+
+    Note that this is just intended for use SXS data versions; for more general
+    use, you probably want something like
+    [`packaging.version`](https://packaging.pypa.io/en/latest/version.html).
+
+    """
+    def __init__(self, v):
+        if "." not in v:
+            v = f"1.{v}"
+        self.major, self.minor = map(int, v.split("."))
+    def __str__(self):
+        return f"{self.major}.{self.minor}"
+    def __repr__(self):
+        return f"{self.major}.{self.minor}"
+    def increment(self, level="minor"):
+        if level=="major":
+            self.major += 1
+        elif level=="minor":
+            self.minor += 1
+        else:
+            raise ValueError("""Can only increment major or minor levels, not "{level}".""")
+        return self
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __lt__(self, other):
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major < other.major or (self.major == other.major and self.minor < other.minor)
+
+    def __le__(self, other):
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major <= other.major or (self.major == other.major and self.minor <= other.minor)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major == other.major and self.minor == other.minor
+
+    def __ge__(self, other):
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major >= other.major or (self.major == other.major and self.minor >= other.minor)
+
+    def __gt__(self, other):
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major > other.major or (self.major == other.major and self.minor > other.minor)
+
+    def __ne__(self, other: object) -> bool:
+        if not isinstance(other, SimpleVersion):
+            return NotImplemented
+        return self.major != other.major or self.minor != other.minor
