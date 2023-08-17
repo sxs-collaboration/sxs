0d74772b340f369649eec892282508f5fd43ecb8
Mike Boyle <michael.oliver.boyle@gmail.com>
2023-08-17 14:58:31 -0400

    Don't bother with `del`

--- sxs/waveforms/preprocessing/__init__.py
+++ sxs/waveforms/preprocessing/__init__.py
@@ -4,6 +4,3 @@
 
 from .taper_functions import smooth_sigmoid, cosine_sigmoid, taper_function
 from .preprocessor import transition_tail_to_constant, pad, taper, detrend, preprocess_general, preprocess
-
-del taper_functions
-del preprocessor
