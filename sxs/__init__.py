b4977755e9476765755d5ce7ac6a80808607eaf7
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-03-14 15:07:47 -0400

    Import WaveformModesDict at highest level

--- sxs/__init__.py
+++ sxs/__init__.py
@@ -20,7 +20,7 @@ from .time_series import TimeSeries
 from .metadata import Metadata
 from .catalog import Catalog
 from .horizons import Horizons, HorizonQuantities
-from .waveforms import WaveformModes #, WaveformGrid, WaveformSignal
+from .waveforms import WaveformModes, WaveformModesDict #, WaveformGrid, WaveformSignal
 from .waveforms import rotating_paired_xor_multishuffle_bzip2 as rpxmb
 from .waveforms import rotating_paired_diff_multishuffle_bzip2 as rpdmb
 from . import catalog, metadata, horizons, waveforms, zenodo, caltechdata
