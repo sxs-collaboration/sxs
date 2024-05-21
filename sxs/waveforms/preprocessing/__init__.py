a04a531976a276b350b765ef9b152ed493c4bf3f
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-05-21 16:20:20 -0400

    Remove files with individual preprocessing functions in favor of methods

--- sxs/waveforms/preprocessing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Functions used for preprocessing
-
-"""
-
-from .taper_functions import smooth_sigmoid, cosine_sigmoid, taper_function
-from .preprocessor import transition_tail_to_constant, pad, taper, detrend, preprocess_general, preprocess
