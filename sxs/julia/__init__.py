2e26888d125bab4313ebf768e30a37806abae713
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-07-25 01:26:28 -0400

    Update PostNewtonian.jl and test interface #minor

--- sxs/julia/__init__.py
+++ sxs/julia/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 import quaternionic
 from .. import WaveformModes
 
@@ -6,7 +7,8 @@ import juliacall
 
 PostNewtonian = juliacall.newmodule("PN")
 PostNewtonian.seval("using PostNewtonian")
-GWFrames = PostNewtonian.GWFrames
+
+from . import GWFrames
 
 def pkg_update():
     """Update all installed Julia packages (but not julia itself)."""
@@ -16,6 +18,7 @@ def pkg_update():
 
 def PNWaveform(
         M1, M2, chi1, chi2, Omega_i, *,
+        inertial=True,
         ell_min=2, ell_max=8, waveform_pn_order=None,
         **orbital_evolution_kwargs
 ):
@@ -33,7 +36,7 @@ def PNWaveform(
         - `v` (array): The orbital velocity as a function of time.
         - `orbital_phase` (array): The orbital phase as a function of
           time.
-    
+
     This is a wrapper around the Julia functions
     `PostNewtonian.orbital_evolution` and
     `PostNewtonian.inertial_waveform`.  See [the Julia
@@ -45,7 +48,7 @@ def PNWaveform(
     docs](https://moble.github.io/PostNewtonian.jl/dev/interface/python/)
     for details.  Also, the GWFrames submodule of this module provides
     another interface.
-    
+
     """
     # Integrate the orbital dynamics
     inspiral = PostNewtonian.orbital_evolution(
@@ -55,9 +58,19 @@ def PNWaveform(
 
     # Compute the waveform in the inertial frame
     waveform_pn_order = waveform_pn_order or PostNewtonian.typemax(PostNewtonian.Int)
-    h = PostNewtonian.inertial_waveform(
-        inspiral, ell_min=ell_min, ell_max=ell_max, PNOrder=waveform_pn_order
-    ).to_numpy().T
+    coorbital_frame = quaternionic.array(values[8:12, :].to_numpy().T)
+    if inertial:
+        frame = np.array([quaternionic.one])
+        frame_type = "inertial"
+        h = PostNewtonian.inertial_waveform(
+            inspiral, ell_min=ell_min, ell_max=ell_max, PNOrder=waveform_pn_order
+        ).to_numpy().T
+    else:
+        frame = coorbital_frame
+        frame_type = "coorbital"
+        h = PostNewtonian.coorbital_waveform(
+            inspiral, ell_min=ell_min, ell_max=ell_max, PNOrder=waveform_pn_order
+        ).to_numpy().T
 
     w = WaveformModes(
         h,
@@ -65,11 +78,15 @@ def PNWaveform(
         modes_axis=1,
         ell_min=ell_min,
         ell_max=ell_max,
+        spin_weight=-2,
+        frame=frame,
+        frame_type=frame_type,
+        data_type="h",
         M1=values[0, :].to_numpy(),
         M2=values[1, :].to_numpy(),
         chi1=values[2:5, :].to_numpy().T,
         chi2=values[5:8, :].to_numpy().T,
-        frame=quaternionic.array(values[8:12, :].to_numpy().T),
+        coorbital_frame=coorbital_frame,
         v=values[12, :].to_numpy(),
         orbital_phase=values[13, :].to_numpy(),
     )
@@ -79,7 +96,7 @@ def PNWaveform(
     w.M2 = w._metadata["M2"]
     w.chi1 = w._metadata["chi1"]
     w.chi2 = w._metadata["chi2"]
-    #w.frame = w._metadata["frame"]  ## Already done
+    w.coorbital_frame = w._metadata["coorbital_frame"]
     w.v = w._metadata["v"]
     w.orbital_phase = w._metadata["orbital_phase"]
 
