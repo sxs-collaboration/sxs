587a99e40762f6c063094b1c84010b8eb7062498
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-08-05 11:56:52 -0400

    Ensure a DeprecationWarning is used

--- sxs/catalog/catalog.py
+++ sxs/catalog/catalog.py
@@ -61,7 +61,7 @@ class Catalog(object):
         favor of the `Simulations` interface.  See the documentation
         for more information.
         """
-        warn(deprecation_notice)
+        warn(deprecation_notice, DeprecationWarning)
 
         progress = read_config("download_progress", True)
 
