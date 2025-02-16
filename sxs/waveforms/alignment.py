Correct factor of 2 in 4d optimization rotation

sxs/waveforms/alignment.py
@@ -525,7 +525,7 @@ def align4d(
     optimum = least_squares(
         cost_wrapper,
         δt_δso3,
-        bounds=[(δt_lower, -np.pi/2, -np.pi/2, -np.pi/2), (δt_upper, np.pi/2, np.pi/2, np.pi/2)],
+        bounds=[(δt_lower, -np.pi, -np.pi, -np.pi), (δt_upper, np.pi, np.pi, np.pi)],
         max_nfev=50000,
     )
     δt = optimum.x[0]