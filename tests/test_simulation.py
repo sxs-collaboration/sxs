fd0eafa00b79a475d53757b352a33dd59eb1b4cb
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-02-24 10:39:13 -0500

    Test v2 loading more thoroughly, including specifying Lev

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,11 +5,36 @@ from .conftest import skip_macOS_GH_actions_downloads
 
 @skip_macOS_GH_actions_downloads
 def test_sxs_load_v2():
-    s = sxs.load("SXS:BBH:0001v2.0")
+    # We'll use SXS:BBH:0001, even though it's deprecated, because we
+    # want to be sure that it always exists, and is accessible as a
+    # v2.0 record.
+    with pytest.warns(UserWarning):
+        s = sxs.load("SXS:BBH:0001v2.0")
     assert s.sxs_id_stem == "SXS:BBH:0001"
     assert s.version == "v2.0"
     s.h
+    s.strain
+    s.H
+    s.Strain
     s.horizons
+    s.Horizons
+    s.psi4
+    s.Psi4
+
+
+@skip_macOS_GH_actions_downloads
+def test_sxs_load_v2_levs():
+    for lev, lev_number in [("", 3), ("/Lev3", 3), ("/Lev2", 2), ("/Lev1", 1)]:
+        s = sxs.load(f"SXS:BBH:1001v2.0{lev}", ignore_deprecation=True)
+        assert s.sxs_id_stem == "SXS:BBH:1001"
+        assert s.version == "v2.0"
+        assert s.lev_number == lev_number
+        s.h
+        s.strain
+        s.H
+        s.Strain
+        s.horizons
+        s.Horizons
 
 
 @skip_macOS_GH_actions_downloads
