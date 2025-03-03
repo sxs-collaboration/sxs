bfe9663b2f8b1bf57cdd6d2c969584d1d8049e7e
Keefe Mitman <kmitman@caltech.edu>
2025-03-03 17:02:40 -0500

    Add missing files and fix text typos

---
+++ tests/test_alignment.py
@@ -0,0 +1,96 @@
+import numpy as np
+import quaternionic
+import pytest
+import sxs
+from .conftest import physicalish_waveform
+
+def test_independent_alignment():
+    h1 = physicalish_waveform()
+    
+    δt = 10 * np.random.rand()
+    δSpin3 = quaternionic.array([np.random.rand(), np.random.rand(), np.random.rand(), np.random.rand()]).normalized
+    
+    h2 = h1.copy()
+    h2.t = h2.t - δt
+    h2 = h2.rotate(δSpin3)
+
+    t1 = -80
+    t2 = 80
+    h1_prime, transformation, norm = sxs.waveforms.alignment.align_waveforms(
+        h1,
+        h2,
+        t1,
+        t2,
+        alignment_method="independent alignment",
+        t_ref=-20
+    )
+    
+    assert np.allclose(norm, 0., atol=1e-6)
+    assert np.allclose([δt, *δSpin3.ndarray], transformation, atol=1e-6)
+    
+def test_align1d():
+    h1 = physicalish_waveform()
+    
+    δt = 10 * np.random.rand()
+    
+    h2 = h1.copy()
+    h2.t = h2.t - δt
+    
+    t1 = -80
+    t2 = 80
+    h1_prime, transformation, norm = sxs.waveforms.alignment.align_waveforms(
+        h1,
+        h2,
+        t1,
+        t2,
+        alignment_method="1d"
+    )
+    
+    assert np.allclose(norm, 0., atol=1e-6)
+    assert np.allclose(δt, transformation[0], atol=1e-6)
+    
+def test_align2d():
+    h1 = physicalish_waveform()
+    
+    δt = 10 * np.random.rand()
+    δSpin3 = quaternionic.array([np.random.rand(), 0., 0., np.random.rand()]).normalized
+    
+    h2 = h1.copy()
+    h2.t = h2.t - δt
+    h2 = h2.rotate(δSpin3)
+
+    t1 = -80
+    t2 = 80
+    h1_prime, transformation, norm = sxs.waveforms.alignment.align_waveforms(
+        h1,
+        h2,
+        t1,
+        t2,
+        alignment_method="2d"
+    )
+    
+    assert np.allclose(norm, 0., atol=1e-6)
+    assert np.allclose([δt, *δSpin3.ndarray], transformation, atol=1e-6)
+    
+def test_align4d():
+    h1 = physicalish_waveform()
+    
+    δt = 10 * np.random.rand()
+    δSpin3 = quaternionic.array([np.random.rand(), np.random.rand(), np.random.rand(), np.random.rand()]).normalized
+    
+    h2 = h1.copy()
+    h2.t = h2.t - δt
+    h2 = h2.rotate(δSpin3)
+
+    t1 = -80
+    t2 = 80
+    h1_prime, transformation, norm = sxs.waveforms.alignment.align_waveforms(
+        h1,
+        h2,
+        t1,
+        t2,
+        alignment_method="4d"
+    )
+    
+    assert np.allclose(norm, 0., atol=1e-6)
+    assert np.allclose([δt, *δSpin3.ndarray], transformation, atol=1e-6)
