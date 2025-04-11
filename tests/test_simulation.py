8d37517fbf00f45cb6a9e44013b8523e2e6f5f38
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-04-11 00:15:35 -0400

    Test various features of sxs.load on v3 simulations

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -50,6 +50,51 @@ def test_sxs_load_v2_levs():
 
 
 @skip_macOS_GH_actions_downloads
+def test_sxs_load_v3():
+    # # This is necessary only after the deprecation of SXS:BBH:4000v3.0
+    # with pytest.warns(UserWarning):
+    #     s = sxs.load("SXS:BBH:4000v3.0")
+    s = sxs.load("SXS:BBH:4000v3.0")
+    assert s.sxs_id_stem == "SXS:BBH:4000"
+    assert s.version == "v3.0"
+    s.h
+    s.strain
+    s.H
+    s.Strain
+    s.horizons
+    s.Horizons
+    s.psi4
+    s.Psi4
+
+
+@skip_macOS_GH_actions_downloads
+def test_sxs_load_v3_levs():
+    # Check that a bad Lev raises an error
+    with pytest.raises(ValueError):
+        sxs.load("SXS:BBH:4000v3.0/Lev189", ignore_deprecation=True)
+    # Check that the default Lev works, and different Levs produce different metadata
+    assert (
+        sxs.load("SXS:BBH:4000v3.0").metadata.reference_time
+        == sxs.load("SXS:BBH:4000v3.0/Lev3").metadata.reference_time
+    )
+    assert (
+        sxs.load("SXS:BBH:4000v3.0/Lev1").metadata.reference_time
+        != sxs.load("SXS:BBH:4000v3.0/Lev3").metadata.reference_time
+    )
+    for lev, lev_number in [("", 3), ("/Lev3", 3), ("/Lev2", 2), ("/Lev1", 1)]:
+        s = sxs.load(f"SXS:BBH:4000v3.0{lev}", ignore_deprecation=True)
+        assert s.sxs_id_stem == "SXS:BBH:4000"
+        assert s.version == "v3.0"
+        assert s.lev_number == lev_number
+        s.h
+        s.strain
+        s.H
+        s.Strain
+        s.horizons
+        s.Horizons
+
+
+@skip_macOS_GH_actions_downloads
 @pytest.mark.parametrize("loader", [sxs.Simulation, sxs.load])
 def test_superseding(loader):
     simulation = "SXS:BBH:0001"
