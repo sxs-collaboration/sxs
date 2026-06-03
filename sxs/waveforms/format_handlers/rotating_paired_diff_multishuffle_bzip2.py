a55da27a3f834ccc94050c54cabf9c33eed52bd6
Aniket Khairnar <akhairna@go.olemiss.edu>
2026-06-03 16:38:30 -0500

    Add multiplication_truncator to load in rpdmb

--- sxs/waveforms/format_handlers/rotating_paired_diff_multishuffle_bzip2.py
+++ sxs/waveforms/format_handlers/rotating_paired_diff_multishuffle_bzip2.py
@@ -567,6 +567,7 @@ def load(
         ell_min=ell_min,
         ell_max=ell_max,
         spin_weight=spin_weight,
+        multiplication_truncator=max
     )
 
     if convert_from_conjugate_pairs:
